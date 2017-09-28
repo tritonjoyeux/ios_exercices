6c3a400d26ef463775224d083fe2daadb5667614
SUP'Internet 15 <supinternet15@macbook-air-de-supinternet-8.local>
2017-09-28 10:10:31 +0200

    Initial Commit

---
+++ Exercices/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Exercices
+//
+//  Created by SUP'Internet 15 on 28/09/2017.
+//  Copyright © 2017 SUP'Internet 15. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
